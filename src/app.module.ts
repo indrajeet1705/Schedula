097c5cfd5d10a2833bffced64d5b9febba9ce4d4
indrajeet bhujabl <indrajeetbhujbal0@gmail.com>
2025-09-19 18:45:40 +0530

    updated main  port

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,13 +24,9 @@ import { JwtAuthGuard } from './auth/guards/jwt.guard';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD')  ,
-        database: configService.get('DB_NAME'),
-        entities: [join(process.cwd(),'dist/**/*.entity.js')],
-        synchronize: true,
+       url:configService.get('DATABASE_URL'),
+       autoLoadEntities:true,
+       synchronize:true
       }),
       inject: [ConfigService],
       
